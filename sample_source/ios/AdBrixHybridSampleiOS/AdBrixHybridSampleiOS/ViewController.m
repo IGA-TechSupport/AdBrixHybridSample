88241459ecf0c49edc15434ed4f438bf862a74be
john-igaw <john@igaworks.com>
2017-11-08 11:14:53 +0900

    iOS 프로젝트 코드 정리 및 주석 추가

--- sample_source/ios/AdBrixHybridSampleiOS/AdBrixHybridSampleiOS/ViewController.m
+++ sample_source/ios/AdBrixHybridSampleiOS/AdBrixHybridSampleiOS/ViewController.m
@@ -48,45 +48,105 @@
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request
  navigationType:(UIWebViewNavigationType)navigationType
 {
-    
-    if ([[[request URL] absoluteString] hasPrefix:@"adbrix:"])
+    if ([[[request URL] scheme] isEqualToString:@"adbrix"])
     {
-        NSString *requestString = [[request URL] absoluteString];
-        NSArray *components = [requestString componentsSeparatedByString:@"://"];
-        NSString *pDataString = [components objectAtIndex:1];
-        NSString *pFunctionName;
-        NSScanner *scanner = [NSScanner scannerWithString:pDataString];
-        [scanner scanUpToString:@"?" intoString:&pFunctionName];
-        
-        NSLog(@"parameters : %@", [self getURLParmaters:[request URL]]);
+     
+        NSString *pFunctionName = [[request URL] host];
+        NSDictionary *adbrixEventDict = [self getURLParmaters:[request URL]];
+        NSLog(@"parameters : %@", adbrixEventDict);
         
         if([pFunctionName containsString:@"fte"])
         {
-            [AdBrix firstTimeExperience: [[self getURLParmaters:[request URL]] valueForKey:@"activity"]];
+            /*!
+             @abstract
+             first time experience의 Activity에 해당할때 호출한다.
+             
+             @param activityName              activity name.(mandatory)
+             */
+            [AdBrix firstTimeExperience: [adbrixEventDict valueForKey:@"activity"]];
         }
         else if([pFunctionName containsString:@"ret"])
         {
-            [AdBrix retention: [[self getURLParmaters:[request URL]] valueForKey:@"activity"]];
+            /*!
+             @abstract
+             retension의 Activity에 해당할때 호출한다.
+             
+             @param activityName              activity name.(mandatory)
+             */
+            [AdBrix retention: [adbrixEventDict valueForKey:@"activity"]];
         }
         else if([pFunctionName containsString:@"purchase"])
         {
-            NSArray* categoryList = [[[self getURLParmaters:[request URL]] valueForKey:@"category"] componentsSeparatedByString:@"."];
+            NSArray* categoryList = [[adbrixEventDict valueForKey:@"category"] componentsSeparatedByString:@"."];
             NSString* categoryString[5];
-            for (int i=0; i<categoryList.count; ++i)
+            if([categoryList count] > 0)
             {
-                categoryString[i] = categoryList[i];
+                for (int i=0; i<categoryList.count; ++i)
+                {
+                    categoryString[i] = categoryList[i];
+                }
             }
             
+            /*!
+             @abstract
+             purchase의 Activity에 해당할때 호출한다.
+             
+             @param string:orderId              order id.(mandatory)
+             @param object:product              CommerceProductMedel's object.(mandatory)
+             @param double:discount             value  of discount.(mandatory)
+             @param double:deliveryCharge       value  of delivery charge.(mandatory)
+             @param string:paymentMethod        payment method. (opnational)
+             @param dictionary:atrrData         extra attrbute data. (opnational)
+             */
+
             
-            [AdBrix commercePurchase:[[self getURLParmaters:[request URL]] valueForKey:@"oid"] product:[AdBrix createCommerceProductModel:[[self getURLParmaters:[request URL]] valueForKey:@"pid"]
-                                                                                     productName:[[self getURLParmaters:[request URL]] valueForKey:@"pname"]
-                                                                                           price:[[[self getURLParmaters:[request URL]] valueForKey:@"price"] doubleValue]
-                                                                                        discount:0.0
-                                                                                        quantity:[[[self getURLParmaters:[request URL]] valueForKey:@"quantity"] integerValue]
-                                                                                  currencyString:[[self getURLParmaters:[request URL]] valueForKey:@"currency_code"]
-                                                                                        category:[AdBrixCommerceProductCategoryModel create:categoryString[0] category2:categoryString[1] category3:categoryString[2] category4:categoryString[3] category5:categoryString[4]]
-                                                                                   extraAttrsMap:nil
-                                                  ] discount:0.0 deliveryCharge:0.0 paymentMethod:@"" atrrData:nil];
+            [AdBrix commercePurchase:[adbrixEventDict valueForKey:@"oid"]
+             
+                            /*!
+                              @abstract
+                              purchase의 CommerceProductMedel을 생성한다.
+                              
+                              @param string:productId            product id.(mandatory)
+                              @param string:productName          product name.(mandatory)
+                              @param double:price                value of price.(mandatory)
+                              @param double:discount             value of discount.(mandatory)
+                              @param int:quantity                value of quantity.(mandatory)
+                              @param string:currencyString       currency.(opnational)
+                              @param object:categories           AdBrixCommerceProductCategoryModel object.(opnational)
+                              @param dictionary:atrrData         extra attrbute data.(opnational)
+                              */
+          
+                             product:[AdBrix createCommerceProductModel:[adbrixEventDict valueForKey:@"pid"]
+                                                            productName:[adbrixEventDict valueForKey:@"pname"]
+                                                                  price:[[adbrixEventDict valueForKey:@"price"] doubleValue]
+                                                               discount:0.0
+                                                               quantity:[[adbrixEventDict valueForKey:@"quantity"] integerValue]
+                                                         currencyString:[adbrixEventDict valueForKey:@"currency_code"]
+                                      
+                                                             /*!
+                                                                   @abstract
+                                                                   AdBrixCommerceProductCategoryModel의 AdBrixCommerceProductCategoryModel을 생성한다.
+                                                                   최대 5개까지 생성할 수 있다.
+                                                                   범주가 큰 카테고리부터 작은 순으로 인자를 집어넣는다
+                                                              
+                                                                   @param string:category1            category.(opnational)
+                                                                   @param string:category2            category.(opnational)
+                                                                   @param string:category3            category.(opnational)
+                                                                   @param string:category4            category.(opnational)
+                                                                   @param string:category5            category.(opnational)
+                                                                   */
+                                                               category:[AdBrixCommerceProductCategoryModel create:categoryString[0]
+                                                                                                         category2:categoryString[1]
+                                                                                                         category3:categoryString[2]
+                                                                                                         category4:categoryString[3]
+                                                                                                         category5:categoryString[4]
+                                                                         ]
+                                                           extraAttrsMap:nil
+                                                  ]
+                            discount:0.0
+                      deliveryCharge:0.0
+                       paymentMethod:[AdBrix paymentMethod:AdBrixPaymentCreditCard]
+                            atrrData:nil];
         }
         return NO;
     }
